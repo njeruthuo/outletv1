6bda4417446f99a1068ef3670ae48504fef68d2b
Julius Njeru <127553760+njeruthuo@users.noreply.github.com>
2024-12-25 07:49:05 +0300

    settings planning

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,3 +1,41 @@
+/**
+ * Password changing setting,
+ * 
+ * Configure Discounts Based on Quantity/Season
+ * Auto-Reorder Levels
+ *
+ * Warehouse/Shop Management:
+      Add/Edit/Delete Warehouse Locations
+      Stock Transfer Between Warehouses
+
+ * Theme and Branding:
+      Set Dashboard Themes
+      Add Custom Branding to Reports
+
+ * Adding / Removing Suppliers
+
+ * Product Categories:
+      Add/Edit/Delete Product Categories
+      Assign Default Categories for New Products
+
+ * Unit of Measurement:
+      Manage Units (e.g., kg, pcs, liters)
+      Default Measurement Units
+ * Backup Settings:
+      Enable Automatic Backups
+      Set Backup Frequency (Daily, Weekly, Monthly)
+  
+ * User Management:
+      Add/Edit/Delete Users
+      Assign Roles (e.g., Admin, Manager, Staff)
+      Permission Levels for Each Role
+
+ * Integration Settings:
+      Export Formats (e.g., CSV, PDF)
+      Scheduled Report Delivery
+
+ */
+
 const Settings = () => {
   return <div>Settings</div>;
 };
